066e16f6b78cd2547b2e7b825ec53d7dd7af1508
KKM3657 <mmm3657@naver.com>
2022-10-26 21:50:42 +0900

    우선순위 큐를 이용한 풀이

--- 더 맵게/Solution.java
+++ 더 맵게/Solution.java
@@ -1,24 +1,20 @@
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Collections;
+import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        LinkedList<Integer> q = new LinkedList<>();
-        for(int i = 0; i<scoville.length; i++){
-            q.add(scoville[i]);
+        PriorityQueue<Integer> heap = new PriorityQueue<>();    // 우선순위 큐 선언
+        for(int i = 0; i<scoville.length; i++){ // 우선순위 큐에 데이터 입력
+            heap.add(scoville[i]);
         }
-        int min = q.get(0);
-        while(min < K){
+        while(heap.peek() < K){ // 스코빌 지수를 K 이상으로 만들때까지 반복
+            if(heap.size() < 2) // 스코빌 지수가 1개 남고 K 이상이 되지 않을 경우
+                return -1;
+            int first = heap.poll();    // 가장 맵지 않은 음식의 스코빌
+            int second = heap.poll();   // 두번째로 맵지 않은 음식의 스코빌
+            int new_value = first + (second * 2);
+            heap.add(new_value);    // 우선순위 큐에 데이터 삽입
             answer++;
-            int first = q.removeFirst();
-            int second = q.removeFirst(); 
-            
-            int new_value = first + second * 2;
-            q.add(new_value);
-            Collections.sort(q);
-            min = q.get(0);
         }
         return answer;
     }
