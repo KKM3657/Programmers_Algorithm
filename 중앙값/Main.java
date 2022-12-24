8feeb0ddf6d04cb1ec468bf31f60e80db2850acc
KKM3657 <mmm3657@naver.com>
2022-12-24 19:36:57 +0900

    코드트리 우선순위 큐

--- 중앙값/Main.java
+++ 중앙값/Main.java
@@ -1,61 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Arrays;
-
 public class Main {
-    public static final int MAX_N = 100000;
-    
-    // 변수 선언
-    public static int t, m;
-    public static int[] arr = new int[MAX_N];
-    
-    public static void findMedian() {
-        int median = arr[0];
-        PriorityQueue<Integer> maxPq = new PriorityQueue<>();
-        PriorityQueue<Integer> minPq = new PriorityQueue<>();
-        System.out.print(median + " ");
-    
-        for(int i = 1; i < m; i++) {
-            if(i % 2 == 1) {
-                if(arr[i] < median)
-                    maxPq.add(-arr[i]);
-                else
-                    minPq.add(arr[i]);
-            }
-            else {
-                // Step 1. 
-                // 먼저 maxPq, minPq 중 개수가 1개 더 많은 쪽에
-                // 있는 곳에서 우선순위가 가장 큰 값을 뽑아줍니다.
-                // 이를 newCand라 하겠습니다.
-                int newCand;
-                if(maxPq.size() > minPq.size())
-                    newCand = -maxPq.poll();
-                else 
-                    newCand = minPq.poll();
-    
-                int[] nums = new int[]{median, arr[i], newCand};
-                Arrays.sort(nums);
-                
-                maxPq.add(-nums[0]);
-                median = nums[1];
-                minPq.add(nums[2]);
-                
-                System.out.print(median + " ");
-            }
-        }
-        
-        System.out.println();
-    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        t = sc.nextInt();
-        while(t-- > 0) {
-            m = sc.nextInt();
-            for(int i = 0; i < m; i++)
-                arr[i] = sc.nextInt();
+        int k = Integer.parseInt(st.nextToken());
+        for(int t=0; t<k; t++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
 
-            findMedian();
+            st = new StringTokenizer(br.readLine());
+            int middle = 0;
+            int target = 0;
+    
+            PriorityQueue<Integer> max_heap = new PriorityQueue<>();    // 중앙값보다 큰 값중에 최소값
+            PriorityQueue<Integer> min_heap = new PriorityQueue<>(Collections.reverseOrder()); // 중앙값보다 작은 값중에 최대값
+            for(int i=1; i<=n; i++){
+                int value = Integer.parseInt(st.nextToken());
+                if(i==1){
+                    middle = value;
+                    System.out.print(middle + " ");
+                    continue;
+                }
+                // 짝수일때
+                if(max_heap.size() == min_heap.size()){
+                    if(middle < value)
+                        max_heap.add(value);
+                    else
+                        min_heap.add(value);
+                    continue;
+                }
+                // 최대 힙의 원소수가 많을때
+                else if(max_heap.size() > min_heap.size())
+                    target = max_heap.poll();
+                else    // 최소 힙의 원소수가 많을때
+                    target = min_heap.poll();
+
+                // 재분배 실시
+                int[] arr = new int[]{target, value, middle};
+                Arrays.sort(arr);
+                min_heap.add(arr[0]);
+                middle = arr[1];
+                max_heap.add(arr[2]);
+                System.out.print(middle + " ");
+            }
+            System.out.println();
         }
     }
 }
