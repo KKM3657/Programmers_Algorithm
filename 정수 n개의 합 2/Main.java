39395c5cb4d6b1714434179a46efecaf2e31082d
김강민 <mmm3657@naver.com>
2022-12-27 00:27:42 +0900

    코드트리 prefix

--- 정수 n개의 합 2/Main.java
+++ 정수 n개의 합 2/Main.java
@@ -1,38 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
 
-public class Main {    
-    public static final int INT_MIN = Integer.MIN_VALUE;
-    public static final int MAX_N = 100000;
-    
-    // 변수 선언
-    public static int n, k;
-    public static int[] arr = new int[MAX_N + 1];
-    public static int[] prefixSum = new int[MAX_N + 1];
-    public static int ans = INT_MIN;
-    
-    // [s, e] 구간 내의 원소의 합을 반환합니다.
-    public static int getSum(int s, int e) {
-        return prefixSum[e] - prefixSum[s - 1];
-    }
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력:
-        n = sc.nextInt();
-        k = sc.nextInt();
-        for(int i = 1; i <= n; i++)
-            arr[i] = sc.nextInt();
+        int[] arr = new int[n];
+        int[] dp = new int[n];
 
-        // 누적합 배열을 만들어줍니다.
-        prefixSum[0] = 0;
-        for(int i = 1; i<= n; i++)
-            prefixSum[i] = prefixSum[i - 1] + arr[i];
-        
-        // 모든 구간에 대해 합을 찾아
-        // 그 중 최댓값을 갱신합니다.
-        for(int i = 1; i <= n - k + 1; i++)
-            ans = Math.max(ans, getSum(i, i + k - 1));
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-        System.out.print(ans);
+        int answer = Integer.MIN_VALUE;
+        dp[0] = arr[0];
+
+        // 누적 합 저장
+        for(int i=1; i<n; i++){
+            dp[i] = dp[i-1] + arr[i];
+        }
+
+        // 구간 별 최대 합
+        for(int i=1; i<n-k+1; i++){
+            answer = Math.max(answer, dp[i+k-1] - dp[i-1]);
+        }
+        System.out.println(answer);
+
     }
 }
