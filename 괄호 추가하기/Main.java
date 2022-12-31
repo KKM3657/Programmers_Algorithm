0da1efa712df80b4d029ff025be77d141b5b1224
김강민 <mmm3657@naver.com>
2023-01-01 01:33:08 +0900

    백준 백트래킹(16637)

--- 괄호 추가하기/Main.java
+++ 괄호 추가하기/Main.java
@@ -5,9 +5,10 @@ import java.util.StringTokenizer;
 import java.util.ArrayList;
 
 public class Main {
-    static int n, answer = 0;
+    static int n, answer = Integer.MIN_VALUE;
     static ArrayList<Integer> arr1 = new ArrayList<>();
     static ArrayList<String> arr2 = new ArrayList<>();
+    // 계산하기
     public static int calculate_value(String operator, int first, int second){
         if(operator.equals("+"))
             return first + second;
@@ -16,14 +17,18 @@ public class Main {
         else
             return first * second;
     }
+    // 괄호 만들기
     public static void find_maxValue(int opIdx, int result){
+        // 끝까지 돈 경우
         if(opIdx >= arr2.size()){
             answer = Math.max(answer, result);
             return;
         }
+        // 괄호가 없는경우 순서대로 계산 후 넘어감
         int value1 = calculate_value(arr2.get(opIdx), result, arr1.get(opIdx+1));
         find_maxValue(opIdx+1, value1);
 
+        // 괄호가 있는경우 뒤에 있는것을 먼저 계산하고 넘어감
         if (opIdx + 1 < arr2.size()) {
             int value2 = calculate_value(arr2.get(opIdx + 1), arr1.get(opIdx+1), arr1.get(opIdx+2));
             find_maxValue(opIdx+2, calculate_value(arr2.get(opIdx), result, value2));
@@ -44,7 +49,7 @@ public class Main {
             arr1.add(Integer.parseInt(token));
         }
         
-        find_maxValue(0,0);
+        find_maxValue(0,arr1.get(0));
         System.out.println(answer);
     }
 }
