8779888865dd5c6b76c510d6125e7116c1bdd585
김강민 <mmm3657@naver.com>
2023-03-21 00:51:21 +0900

    백준 이분탐색(1300)

--- K번째 수/Main.java
+++ K번째 수/Main.java
@@ -7,18 +7,30 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
         
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=N; j++){
-                pq.add(i*j);
-            }
-        }
-
-        for(int i=1; i<k; i++){
-            pq.poll();
-        }
-        System.out.println(pq.poll());
-    }
+        long lo = 1;
+		long hi = K;
+		
+		// lower-bound
+		while(lo < hi) {
+			
+			long mid = (lo + hi) / 2;	// 임의의 x(mid)를 중간 값으로 잡는다.
+			long count = 0;
+			
+			for(int i = 1; i <= N; i++) {
+				count += Math.min(mid / i, N);
+			}
+			
+			// count가 많다는 것은 임의의 x(mid)보다 작은 수가 B[K]보다 많다는 뜻
+			if(K <= count) {
+				hi = mid;
+			}
+			else {
+				lo = mid + 1;
+			}
+		}
+		
+		System.out.println(lo);
+	}
 }
