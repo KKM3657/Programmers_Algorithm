0b219589ccfb423bfeb9849c18e279feb28658c0
김강민 <mmm3657@naver.com>
2023-05-18 00:45:53 +0900

    백준 dfs(14503)

--- 로봇 청소기/Main.java
+++ 로봇 청소기/Main.java
@@ -3,41 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Pair{
-    int x;
-    int y;
-
-    Pair(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
 public class Main {
-    static int N, M, dir, answer = 0;
+    static int N, M, dir, answer = 1;
     static int[][] grid;
-    static int[] dx = new int[]{-1,0,1,0}, dy = new int[]{0,-1,0,1};
-    static char[] pic = new char[]{'^','<','v','>'};
+    static int[] dx = new int[]{-1,0,1,0}, dy = new int[]{0,1,0,-1};
     static char[][] graph;
     public static void dfs(int cx, int cy){
         // 청소
         grid[cx][cy] = 2;
-        graph[cx][cy] = pic[dir];
-        answer++;
+
         // 4방향
         for(int i=0; i<4; i++) {
+            dir = (dir + 3) % 4; // 방향 전환
             int nx = cx + dx[dir], ny = cy + dy[dir];
-            if(0 > nx || nx >= N || 0 > ny || ny >= M || grid[nx][ny] != 0)
-                dir = (dir + 1) % 4;
-            if(grid[nx][ny] == 0)
+            if(0 <= nx && nx < N && 0 <= ny && ny <= M && grid[nx][ny] == 0) { // 이동
+                answer++;
                 dfs(nx, ny);
+                return;
+            }
         }
-        dir = (dir + 1) % 4;
-        int nx = cx - dx[dir], ny = cy - dy[dir];
-        if(grid[nx][ny] == 1){
-            System.out.println(answer);
-            System.exit(0);
+        int nx = cx - dx[dir], ny = cy - dy[dir];   // 방향 유지한 상태에서 뒤로
+        if(grid[nx][ny] != 1){
+            dfs(nx,ny); // 다음으로 이동
         }
-
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -46,7 +34,6 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        //start = new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         int sx = Integer.parseInt(st.nextToken()), sy = Integer.parseInt(st.nextToken());
         dir = Integer.parseInt(st.nextToken());
 
@@ -59,5 +46,6 @@ public class Main {
             }
         }
         dfs(sx, sy);
+        System.out.println(answer);
     }
 }
